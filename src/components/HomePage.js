406161af48d0c721f2849a57b7eb36ec7b1711df
jay270804 <jaypatel270804@gmail.com>
2024-12-23 16:46:38 +0530

    feature/ added BACKEND_URL to .env file to set it dynamically

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,33 +1,3 @@
-// import React, {useEffect, useState} from "react"
-// import api from "../services/api";
-// import { useNavigate } from "react-router-dom"
-
-// const HomePage = () =>{
-//     const[sessionId, setSessionId] = useState(null);
-//     const navigate = useNavigate();
-
-//     useEffect(() => {
-//         if (!localStorage.getItem('session_id')){
-//             api.post('quiz/')
-//             .then((res)=>{
-//                 setSessionId(res.data.session_id);
-//                 localStorage.setItem('session_id', res.data.session_id)
-//             })
-//             .catch((err)=> alert(`Error starting quiz session: ${err.response.data.error}`))
-//         }
-//     }, []);
-
-//     return (
-//         <div>
-//             <h1>Welcome to the Quiz App</h1>
-//             <p>Starting your quiz session...</p>
-//             <button onClick={()=> navigate('/random-question')}>Start Quiz</button>
-//             <button onClick={()=> navigate('/session-stats')}>See your stats</button>
-//         </div>
-//     );
-// };
-
-// export default HomePage;
 import React, { useState, useEffect } from 'react';
 import api from '../services/api';
 import { useNavigate } from 'react-router-dom';
